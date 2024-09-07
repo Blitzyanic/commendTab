3e268307b21efde03525794003a2b90ea160863c
yanic <yanic.dev@gmail.com>
2024-09-07 14:58:18 +0200

    add search placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Clock from "./home/clock/Clock.tsx";
+import Search from "./home/Search/search.tsx";
 
 function App() {
   return (
     <div className="backdrop-blur w-screen h-screen flex justify-center">
       <div className="mt-10">
         <Clock />
+        <Search />
       </div>
     </div>
   );
---
+++ src/home/Search/search.tsx
@@ -0,0 +1,11 @@
+import "./search.css"
+
+function Search() {
+    return (
+        <span className="flex justify-center mt-52 bg rounded">
+            <p>Search</p>
+        </span>
+    );
+}
+
+export default Search;
