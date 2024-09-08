8aa3b72bfec855dbc028a2b163a504ed6121e919
yanic <yanic.dev@gmail.com>
2024-09-08 15:20:57 +0200

    add brave search function

--- src/home/Search/search.tsx
+++ src/home/Search/search.tsx
@@ -1,14 +1,27 @@
 import "./search.css";
 
 function Search() {
+
+  window.addEventListener("keyup", function (event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      let inp = document.getElementById("search-input") as HTMLInputElement;
+      searchBrave(inp.value);
+    }
+  })
+
   return (
-    <div className="flex justify-center mt-52">
+    <div className="mt-52 flex justify-center">
       <input type="text"
-             className="search absolute rounded-md p-1"
+             id="search-input"
+             className="search absolute rounded-md p-1 w-80 h-10"
              autoFocus={true}
       />
     </div>
   );
+}
+
+function searchBrave(question: string) {
+  window.location.replace("https://search.brave.com/search?q="+question);
 }
 
 export default Search;
