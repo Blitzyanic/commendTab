f16021d61456d9aa27d90cf2fed0abe052a3ac1f
yanic <yanic.dev@gmail.com>
2024-09-08 15:34:57 +0200

    add first commend

--- src/home/Search/search.tsx
+++ src/home/Search/search.tsx
@@ -1,11 +1,17 @@
 import "./search.css";
 
-function Search() {
+let sendTo = (url: string) => { window.location.replace(url) };
 
+function Search() {
   window.addEventListener("keyup", function (event: KeyboardEvent) {
     if (event.key === "Enter") {
       let inp = document.getElementById("search-input") as HTMLInputElement;
-      searchBrave(inp.value);
+
+      if (inp.value.charAt(0) === "!") {
+        commend(inp.value.substring(1));
+      } else {
+        searchBrave(inp.value);
+      }
     }
   })
 
@@ -13,15 +19,21 @@ function Search() {
     <div className="mt-52 flex justify-center">
       <input type="text"
              id="search-input"
-             className="search absolute rounded-md p-1 w-80 h-10"
+             className="search absolute rounded-md p-1 w-80 h-10 selection:bg-violet-700/40 selection:text-fuchsia-200"
              autoFocus={true}
       />
     </div>
-  );
+  )
 }
 
 function searchBrave(question: string) {
-  window.location.replace("https://search.brave.com/search?q="+question);
+  sendTo("https://search.brave.com/search?q="+question);
+}
+
+function commend(args: string) {
+  if (args === "yt") {
+    sendTo("https://www.youtube.com");
+  }
 }
 
 export default Search;
