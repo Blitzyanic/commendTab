177c715f98d2cc30b65a5390f5a30a15b6db72a0
yanic <yanic.dev@gmail.com>
2024-09-25 18:02:43 +0200

    remove popup with previews inputs

--- src/home/Search/search.tsx
+++ src/home/Search/search.tsx
@@ -15,7 +15,7 @@ function Search() {
   return (
     <div className="mt-52 flex justify-center">
       <input type="text"
-             autoComplete="false"
+             autoComplete="off"
              id="search-input"
              className="search text-lg absolute rounded-md p-1 w-80 h-10 selection:bg-violet-700/40 selection:text-fuchsia-200"
              autoFocus={true}
