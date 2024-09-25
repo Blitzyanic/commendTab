98ef300eb28b9d356006996791db8f4dc101c343
Doepner, Yanic <yanic.doepner@sulzer.de>
2024-09-25 09:31:08 +0200

    rework shortcuts.ts

--- src/home/Search/shortcuts.ts
+++ src/home/Search/shortcuts.ts
@@ -4,9 +4,5 @@ interface IShortcut {
 
 export const data: IShortcut =  {
     "!yt": "https://youtube.com",
-    "!gh": "https://github.com",
-    "!zeit": "https://portal.sulzer.de/zeit/",
-    "!git": "https://git.sulzer.de/",
-    "!duck": "https://ente.sulzer.de/",
-    "!sulzer": "https://sulzerde.sharepoint.com/"
+    "!gh": "https://github.com"
 }
