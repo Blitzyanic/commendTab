3ac5fbcc674f5e05a1b40c2ae664b82d334500f4
Yanic <94384173+Blitzyanic@users.noreply.github.com>
2024-09-25 09:36:32 +0200

    Update shortcuts.ts

--- src/home/Search/shortcuts.ts
+++ src/home/Search/shortcuts.ts
@@ -5,8 +5,5 @@ interface IShortcut {
 export const data: IShortcut =  {
     "!yt": "https://youtube.com",
     "!gh": "https://github.com",
-    "!zeit": "https://portal.sulzer.de/zeit/",
-    "!git": "https://git.sulzer.de/",
-    "!duck": "https://ente.sulzer.de/",
-    "!sulzer": "https://sulzerde.sharepoint.com/"
+    "!1d20": "https://roll20.net/"
 }
